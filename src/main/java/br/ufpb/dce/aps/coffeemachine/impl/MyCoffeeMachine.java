e317901839264a739fa903e4f1a0351bd3e6c0b2
Danilo Raniery <danilo.coutinho@dce.ufpb.br>
2014-07-14 09:46:03 -0300

    Task 1.01 finished for OO

---
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -0,0 +1,13 @@
+package br.ufpb.dce.aps.coffeemachine.impl;
+
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Display;
+
+public class MyCoffeeMachine implements CoffeeMachine{
+
+	
+	public MyCoffeeMachine (ComponentsFactory factory){
+		factory.getDisplay().info ("Insert coins and select a drink!");
+	}
+}
