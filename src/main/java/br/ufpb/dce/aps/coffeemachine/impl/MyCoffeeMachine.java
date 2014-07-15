a15d207cba8fae4846368cf0ef0d042a0f64b2dc
Danilo Raniery <danilo.coutinho@dce.ufpb.br>
2014-07-15 09:13:54 -0300

    Task 1.02 finished for oo

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,13 +1,25 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Display;
 
 public class MyCoffeeMachine implements CoffeeMachine{
 
+	int total;
 	
+	private ComponentsFactory factory;
+
 	public MyCoffeeMachine (ComponentsFactory factory){
+		this.factory = factory;
 		factory.getDisplay().info ("Insert coins and select a drink!");
+	}
+
+	public void insertCoin(Coin coin) {
+		total += coin.getValue();
+		int valor = total /100;
+	    int centavos = total % 100;
+		factory.getDisplay().info ("Total: US$ "+valor+"." + centavos);
 	}
 }
--- src/test/java/br/ufpb/dce/aps/coffeemachine/MyCoffeeMachineTest.java
+++ src/test/java/br/ufpb/dce/aps/coffeemachine/MyCoffeeMachineTest.java
@@ -5,7 +5,6 @@ import br.ufpb.dce.aps.coffeemachine.impl.MyCoffeeMachine;
 public class MyCoffeeMachineTest extends CoffeeMachineTest{
 
 		
-	@Override
 	protected CoffeeMachine createFacade(ComponentsFactory factory) {
 		return new MyCoffeeMachine(factory);
 	}
