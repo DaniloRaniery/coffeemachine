88504f5a18d1994174db0ff352b9fe45a187253d
Danilo Raniery <danilo.coutinho@dce.ufpb.br>
2014-07-21 08:41:17 -0300

    Task 1.06 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,5 +1,6 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import br.ufpb.dce.aps.coffeemachine.CashBox;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
@@ -8,10 +9,11 @@ import br.ufpb.dce.aps.coffeemachine.Display;
 
 public class MyCoffeeMachine implements CoffeeMachine{
 
-	int total;
-	
+	private int total;
 	private ComponentsFactory factory;
-
+	private CashBox cashBox;
+	private Coin coin;
+	
 	public MyCoffeeMachine (ComponentsFactory factory){
 		this.factory = factory;
 		factory.getDisplay().info ("Insert coins and select a drink!");
@@ -20,6 +22,7 @@ public class MyCoffeeMachine implements CoffeeMachine{
 	public void insertCoin(Coin coin) throws CoffeeMachineException{
 		try{
 			this.total += coin.getValue();
+			this.coin = coin;
 			this.factory.getDisplay().info ("Total: US$ "+this.total/100+"." + this.total%100);
 		}
 		catch(NullPointerException e){
@@ -30,6 +33,11 @@ public class MyCoffeeMachine implements CoffeeMachine{
 	public void cancel() throws CoffeeMachineException {
 		if( this.total == 0){
 			throw new CoffeeMachineException("Não houve moeda inserida"); 	
+		}
+		else{
+			factory.getDisplay().warn ("Cancelling drink. Please, get your coins.");
+			factory.getCashBox().release(coin);
+			factory.getDisplay().info ("Insert coins and select a drink!");
 		}
 	}
 }
