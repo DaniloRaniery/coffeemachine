1d27bb5b581bc3dfb76a855582ff800222a055ad
Danilo Raniery <danilo.coutinho@dce.ufpb.br>
2014-08-18 09:33:12 -0300

    Task 3.01 finished for oo

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -4,6 +4,7 @@ import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Drink;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
@@ -29,5 +30,9 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	public void setFactory(ComponentsFactory factory) {
 		this.factory = factory;		
 		this.gerenteDeMaquina.apresentarMensagemInicial(factory);
+	}
+
+	public void readBadge(int badgeCode) {
+		this.factory.getDisplay().info(Messages.BADGE_READ);
 	}
 }
